6a6fa3908cf52a9c828386eeca9ba0224fd7e6b5
vincent <vincent.gbs11@gmail.com>
2021-12-31 11:46:50 -0500

    Starting login pages

--- setup.sh
+++ setup.sh
@@ -73,3 +73,35 @@ echo "
 .wrapper {
     padding: 20px;
 }" >> src/components/App/App.css
+
+mkdir src/components/Login
+
+echo "import React from 'react';
+import './Login.css';
+
+export default function Login() {
+  return(
+    <div className=\"login-wrapper\">
+      <h1>Please Log In</h1>
+      <form>
+        <label>
+          <p>Username</p>
+          <input type=\"text\" />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type=\"password\" />
+        </label>
+        <div>
+          <button type=\"submit\">Submit</button>
+        </div>
+      </form>
+    </div>
+  )
+}" > src/components/Login/Login.js
+
+echo ".login-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}" > src/components/Login/Login.css
