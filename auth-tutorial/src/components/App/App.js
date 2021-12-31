fb3e22b20982e04ba9a5133fd06713c9ae84e734
vincent <vincent.gbs11@gmail.com>
2021-12-31 11:52:38 -0500

    Adding login page and basic check for token

--- auth-tutorial/src/components/App/App.js
+++ auth-tutorial/src/components/App/App.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from '../Dashboard/Dashboard';
+import Login from '../Login/Login';
 import Preferences from '../Preferences/Preferences';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Application</h1>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/preferences" element={<Preferences />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    const [token, setToken] = useState();
+    if(!token) {
+        return <Login setToken={setToken} />
+    }
+    return (
+        <div className="App">
+            <h1>Application</h1>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/preferences" element={<Preferences />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
