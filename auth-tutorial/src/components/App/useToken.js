628f4e8f997b340afdf8c25fb64eddf1bbcf4482
vincent <vincent.gbs11@gmail.com>
2021-12-31 13:23:55 -0500

    Adding options to use Token

--- auth-tutorial/src/components/App/useToken.js
+++ auth-tutorial/src/components/App/useToken.js
@@ -1,20 +1,29 @@
 import { useState } from 'react';
 
 export default function useToken() {
-  const getToken = () => {
-    const tokenString = sessionStorage.getItem('token');
-    const userToken = JSON.parse(tokenString);
-    return userToken?.token
-  };
-  const [token, setToken] = useState(getToken());
+    const getToken = () => {
+        let tokenString;
+        if (true) {
+            tokenString = sessionStorage.getItem('token');
+        } else {
+            tokenString = localStorage.getItem('token');
+        }
+        const userToken = JSON.parse(tokenString);
+        return userToken?.token
+    };
+    const [token, setToken] = useState(getToken());
 
-  const saveToken = userToken => {
-     sessionStorage.setItem('token', JSON.stringify(userToken));
-     setToken(userToken.token);
-   };
+    const saveToken = userToken => {
+        if (true) {
+            sessionStorage.setItem('token', JSON.stringify(userToken));
+        } else {
+            localStorage.setItem('token', JSON.stringify(userToken));
+        }
+        setToken(userToken.token);
+    };
 
-   return {
-   setToken: saveToken,
-   token
+    return {
+        setToken: saveToken,
+        token
     }
 }
