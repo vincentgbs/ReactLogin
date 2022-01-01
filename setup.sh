fafc965a8675c1e1231c41832fe6e73ad4f9909a
vincent <vincent.gbs11@gmail.com>
2022-01-01 14:49:55 -0500

    Starting https://www.bezkoder.com/react-jwt-auth/ changes

--- setup.sh
+++ setup.sh
@@ -11,14 +11,11 @@ sudo apt install npm -y ## 8.1.2
 sudo npm install -g n ## update node
 sudo n stable ## v16.13.1
 
-## https://www.digitalocean.com/community/tutorials/how-to-manage-state-on-react-class-components
-# npx create-react-app auth-tutorial -y
-# npm start ## to preview
-## preview at: 192.168.33.50:3000
-
 ## https://www.digitalocean.com/community/tutorials/how-to-add-login-authentication-to-react-applications
 cd /vagrant/auth-tutorial/
 npm install react-router-dom
+npm install react-bootstrap bootstrap@5.1.3
+npm install bootstrap
+npm install axios
 
-# npm install react-bootstrap bootstrap@5.1.3
-# npm install bootstrap
+# npm start ## preview at: 192.168.33.50:3000
